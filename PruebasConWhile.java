dda4c6a5f68ff0387af2807b3a6437750aba5abc
Jorge Jaular Lasaga <jordijaulas13@gmail.com>
2018-01-16 18:52:49 +0100

    Se ha agregado un método que imprime por pantalla todos los múltiplos de 5.

---
+++ PruebasConWhile.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class PruebasConWhile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PruebasConWhile
+{    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void multiplosDeCinco()
+    {
+        int contador = 10;
+        while (contador <= 95) {
+            System.out.println(contador);
+            contador += 5;
+        }
+    }
+}
